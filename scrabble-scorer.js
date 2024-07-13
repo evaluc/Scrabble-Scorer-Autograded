1faf1bb5dd629bf967dfc3f9e7dcce730e9c7c16
evaluc <ealuckow@gmail.com>
2024-07-13 14:22:49 -0500

    Fix runtime error by setting parameter defaults and other incremental changes

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,7 +12,7 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
-function oldScrabbleScorer(word) {
+function oldScrabbleScorer(word = "") {
 	word = word.toUpperCase();
 	let letterPoints = "";
  
@@ -35,36 +35,38 @@ function oldScrabbleScorer(word) {
 function initialPrompt() {
    console.log("Let's play some scrabble!");
    let userWord = input.question("Enter a word to score: ");
-
+   //Need to return userWord?
    console.log(vowelBonusScorer(userWord));
 };
 
-let newPointStructure;
-//Take note of this declared variable
+let newPointStructure = transform(oldPointStructure);
+//Take note of this declared variable, it's an object
 
-function simpleScorer(word){
-   word = word.toUpperCase();
-	let letterPoints = "";
- for (let i = 0; i < word.length; i++) {
-   let simplePointValue = 1;		
-	letterPoints += `Points for '${word[i]}': ${simplePointValue}\n`
-	  }
-	return letterPoints;
+function simpleScorer(word = "") {
+   let wordCased = word.toUpperCase();
+	let wordScore = 0;
+   let simplePointValue = 1;
+
+   wordScore = wordCased.length * simplePointValue;
+	
+	return wordScore
 };
 
-function vowelBonusScorer(word) {
-   word = word.toUpperCase();
+//simplified simpleScorer function
+
+function vowelBonusScorer(word = "") {
+   wordCased = word.toUpperCase();
 	let letterPoints = "";
  for (let i = 0; i < word.length; i++) {
    let vowels = ["A","E","I","O","U"];
    let vowelBonusPointValue;
    //The syntax/logic here isn't working, vowels aren't counted as 3
-   if (word[i].indexOf(vowels) > -1) {
+   if (wordCased[i].indexOf(vowels) > -1) {
       vowelBonusPointValue = 3;
    } else {
       vowelBonusPointValue = 1;
    }
-	letterPoints += `Points for '${word[i]}': ${vowelBonusPointValue}\n`
+	letterPoints += `Points for '${wordCased[i]}': ${vowelBonusPointValue}\n`
 	  }
 	return letterPoints;
 };
@@ -77,31 +79,45 @@ let scrabbleScorer;
 let simpleScoreObj = {
    name: "Simple Score",
    description: "Each letter is worth 1 point.",
-   scorerFunction: simpleScorer()
+   scorerFunction: simpleScorer(),
 };
 
 let vowelBonusScoreObj = {
    name: "Bonus Vowels",
    description: "Vowels are 3 pts, consonents are 1 pt.",
-   scorerFunction: vowelBonusScorer()
+   scorerFunction: vowelBonusScorer(),
 };
 
 let scrabbleScoreObj = {
-   name: "The traditional scoring algorithm",
+   name: "Scrabble",
    description: "The traditional scoring algorithm.",
-   scorerFunction: oldScrabbleScorer()
+   scorerFunction: oldScrabbleScorer(),
 };
 
 const scoringAlgorithms = [simpleScoreObj, vowelBonusScoreObj, scrabbleScoreObj];
 
-function scorerPrompt() {}
+function scorerPrompt() {
+   console.log("Which scoring algorithm would you like to use?");
+   console.log(); //insert object literal here accessing the object properties
+   let scoreSelection = input.question("Enter 0, 1, or 2: ");
+   //fill out here to prompt scorer choice, runs after initialPrompt
+
+   return //scoring object
+};
 
 function transform() {};
 
 function runProgram() {
+   // console.log(vowelBonusScoreObj);
    initialPrompt();
+   //scorerPrompt();
+   //Follow up with choosing which algorithm
+   //then run the scoring algorithm on the word and print that word
    
 }
+
+//Fixed error preventing anything from running, other small changes
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
